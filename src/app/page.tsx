05414f7447cc3b46387f45e308a46cb90eb1262d
suzuryo <suzuryo@gmail.com>
2023-03-16 13:30:30 +0900

    color: iwdd を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,22 @@ export default function Home() {
           alt="IWDD Logo"
           width="384"
           height="90"
+          className="opacity-80"
         />
       </h1>
-      <h2 className="text-2xl font-bold underline">
+      <h2 className="text-xl text-iwdd">
         IWDD (vol.183) / オンライン開催 14:00〜
       </h2>
-      <dl>
-        <dt>会場</dt>
+      <dl className="text-sm">
+        <dt className="text-iwdd">会場</dt>
         <dd>オンライン開催 ()</dd>
-        <dt>開催日</dt>
+        <dt className="text-iwdd">開催日</dt>
         <dd>2022.03.12 14:00 - 17:00</dd>
-        <dt>参加費</dt>
+        <dt className="text-iwdd">参加費</dt>
         <dd>社会人500円・学生無料</dd>
-        <dt>今月のお題</dt>
+        <dt className="text-iwdd">今月のお題</dt>
         <dd>募集中</dd>
-        <dt>参加申し込み</dt>
+        <dt className="text-iwdd">参加申し込み</dt>
         <dd>
           <Link href="https://iwdd.connpass.com/event/198964/">
             https://iwdd.connpass.com/event/198964/
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', '!./node_modules'],
   theme: {
+    colors: {
+      iwdd: '#c70d02',
+    },
     extend: {
       fontFamily: {
         shipporiMincho: ['var(--font-shipporiMincho)'],
