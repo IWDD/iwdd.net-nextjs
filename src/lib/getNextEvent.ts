418cde65b7915aededa4f8b4ae6fc03ea839473b
suzuryo <suzuryo@gmail.com>
2025-04-12 18:58:46 +0900

    use opennext cloudflare

--- src/lib/getNextEvent.ts
+++ src/lib/getNextEvent.ts
@@ -1,15 +1,13 @@
 import dayjs from 'dayjs'
-import fs from 'fs'
-import yaml from 'js-yaml'
 
+import data from '@/data.json'
 import { type DataEvent, type DataEvents } from '@/types/DataEvents'
 
 export const getNextEvent = async (): Promise<DataEvent | undefined> => {
-  const file = fs.readFileSync('data.yml', 'utf8')
-  const data = yaml.load(file) as DataEvents
+  const parsedData = data as DataEvents
 
   // Sort events by start_at date
-  const sortedEvents = data?.events?.sort((a, b) =>
+  const sortedEvents = parsedData?.events?.sort((a, b) =>
     dayjs(a.start_at).isAfter(dayjs(b.start_at)) ? 1 : -1,
   )
 
